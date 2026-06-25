4031ca93ef66b1d0fff15b7296683c74825d75b0
Kiran Handi <kiranhandi232@gmail.com>
2026-06-25 23:06:45 +0530

    Update websites.js

--- websites.js
+++ websites.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabaseService = require('../services/supabaseService');
+const supabaseService = require('./supabaseService');
 const slugify = require('../utils/slugify');
 
 /**
